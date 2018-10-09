aaaea3a85eda502b1fe65d8519124df75b176504
jiangjg <jiangjg@moredian.com>
2018-10-09 17:30:51 +0800

    add

---
+++ src/components/Hoc/index.js
@@ -0,0 +1,3 @@
+import Hoc from './Hoc'
+
+module.exports = Hoc
--- src/pages/PageHoc/index.js
+++ src/pages/PageHoc/index.js
@@ -1,4 +1,4 @@
-import Hoc from '../../components/Hoc/Hoc'
+import Hoc from '../../components/Hoc'
 import WrapComp from './PageHoc'
 
 module.exports = Hoc(WrapComp, { title: '这是一段描述信息' })
